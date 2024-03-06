31a888642185ed722834e8454b099b6571ab48eb
unknown <kenth1977@gmail.com>
2024-03-05 20:20:20 -0600

    Mostarndo ambas contraseñas con js y darkmode

--- static/js/eye.js
+++ static/js/eye.js
@@ -1,31 +1,17 @@
 function myFunction() {
-  var x = document.getElementById("pass");
-
-  icon = document.querySelector("i.icon");
-  if (x.type === "password") {
-    x.type = "text";
-    icon.classList.remove("icon-eye")
-    icon.classList.add("icon-eye-blocked")
-  } else {
-    x.type = "password";
-    icon.classList.add("icon-eye")
-  	icon.classList.remove("icon-eye-blocked")
-  }
-}
-
-
-
-function myFunction1() {
+var x = document.getElementById("pass");
   var i = document.getElementById("pass1");
   var o = document.getElementById("pass2");
  
   icon = document.querySelector("i.icon");
-  if (i.type === "password" && o.type === "password") {
+  if(i.type === "password" && o.type === "password" || x.type === "password") {
+    x.type = "text";
     i.type = "text";
     o.type = "text";
     icon.classList.remove("icon-eye")
     icon.classList.add("icon-eye-blocked")
-  } else {
+  }else {
+  	x.type = "password";
     i.type = "password";
     o.type = "password";
     icon.classList.add("icon-eye")
